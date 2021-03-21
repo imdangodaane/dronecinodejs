ea4f15c046254dfa5fb3f3413357f42f18aefbd2
nquizx <nquizx@gmail.com>
2021-03-21 13:29:35 +0000

    Update cho Hằng.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const main = () => {
   console.log('Quí đẹp chai quá mọi người ơi!!');
-  console.log('Thêm 1 dòng để mợi người biết tui đẹp trai thiệt á!')
+  console.log('Thêm 1 dòng để mợi người biết tui đẹp trai thiệt á!');
+  console.log('Hằng đẹp gái quá má ui!!!');
 };
 
 main();
