af3d94a97c1351e885cabba3c9dc9f3bc78f43ea
Qui Nguyen <nquizx@gmail.com>
2022-02-26 12:34:17 +0700

    MKVN-1161: Add new console log

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ const main = () => {
   console.log('Quí đẹp chai quá mọi người ơi!!');
   console.log('Thêm 1 dòng để mợi người biết tui đẹp trai thiệt á!');
   console.log('Hằng đẹp gái quá má ui!!!');
+  console.log('Qui new commit!');
 };
 
 main();
